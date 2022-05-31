977065eb19dd795ebc93fe4cd395b4e06a3510a1
Kolosetta <kolosovskiy91@gmail.com>
2022-06-01 00:15:39 +0300

    Добавлена загрузка из сети и добавление данных в RecyclerView

--- app/src/main/java/com/example/employeessimple/MainActivity.java
+++ app/src/main/java/com/example/employeessimple/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.employeessimple;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.employeessimple.adapter.EmployeesAdapter;
+import com.example.employeessimple.api.ApiFactory;
+import com.example.employeessimple.api.ApiService;
+import com.example.employeessimple.pojo.Employee;
+import com.example.employeessimple.pojo.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    EmployeesAdapter employeesAdapter;
+    Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        employeesAdapter = new EmployeesAdapter();
+        employeesAdapter.setEmployeesList(new ArrayList<>());
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(employeesAdapter);
+
+        ApiFactory apiFactory = ApiFactory.getInstance();
+        ApiService apiService = apiFactory.getApiService();
+        disposable = apiService.getEmployees()
+                .subscribeOn(Schedulers.io()) //Указываем поток загрузки данных
+                .observeOn(AndroidSchedulers.mainThread()) //Указываем прием данных в главном потоке
+                .subscribe(new Consumer<Response>() {
+                    @Override
+                    public void accept(Response response) throws Exception { // Метод срабатывает при успешном приеме данных
+                        employeesAdapter.setEmployeesList(response.getResponse());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Toast.makeText(MainActivity.this, "Ошибка", Toast.LENGTH_LONG ).show();
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(disposable != null) {
+            disposable.dispose(); //Уничтожаем процесс, если он незавершен
+        }
     }
 }
--- app/src/main/java/com/example/employeessimple/adapter/EmployeesAdapter.java
+++ app/src/main/java/com/example/employeessimple/adapter/EmployeesAdapter.java
@@ -1,35 +1,57 @@
 package com.example.employeessimple.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.employeessimple.R;
+import com.example.employeessimple.pojo.Employee;
+import java.util.List;
+
 public class EmployeesAdapter extends RecyclerView.Adapter<EmployeesAdapter.EmployeesViewHolder> {
 
-
+    private List<Employee> employees;
 
     @NonNull
     @Override
     public EmployeesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new EmployeesViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull EmployeesViewHolder holder, int position) {
-
+        Employee employee = employees.get(position);
+        holder.secondName.setText(employee.getlName());
+        holder.firstName.setText(employee.getfName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return employees.size();
     }
 
     static class EmployeesViewHolder extends RecyclerView.ViewHolder{
+        private TextView firstName;
+        private TextView secondName;
 
         public EmployeesViewHolder(@NonNull View itemView) {
             super(itemView);
+            firstName = itemView.findViewById(R.id.firstName);
+            secondName = itemView.findViewById(R.id.secondName);
         }
+    }
+
+    public void setEmployeesList(List<Employee> employees){
+        this.employees = employees;
+        notifyDataSetChanged();
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
     }
 }
